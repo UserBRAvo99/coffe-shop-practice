3c9cc7059831370b6444bdc729d3da8e58547737
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-11-01 11:19:53 +0200

    color

---
+++ src/shared/color.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    darkGreen: "2D635E",
+    lightGreen: "E3EBE7",
+    grey: "444A4A",
+    black: "000000",
+    white: "FFFFFF",
+    beige: "FAE9E0",
+  },
+};
+
+//   colorDarkGreen: "2D635E",
+//   colorLightGreen: "E3EBE7",
+//   colorGrey: "444A4A",
+//   colorBlack: "000000",
+//   colorWhite: "FFFFFF",
+//   colorBeige: "FAE9E0",
